cd874f847695e6b1670ed589f71a8a03e951e61b
sreyalakshmi <sreyalakshmisaji@gmail.com>
2024-06-13 14:43:04 +0530

    navbar

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+    const[data,changedata]=useState(
+        {
+            "email":"",
+            "password":""
+        }
+    )
+    const inputHandler=(event)=>{
+        changedata({...data,[event.target.name]:event.target.value},[])
+    }
+    const readvalue=()=>{
+        console.log(data)
+        axios.post("http://localhost:8080/signin",data).then(
+            (response)=>{
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("successfully added")
+                } else {
+                    alert("error")
+                }
+            }
+        ).catch(
+            (error)=>{
+                console.log(error.message)
+            }
+        )
+    }
   return (
     <div>
         <div className="container">
@@ -9,16 +37,18 @@ const Login = () => {
                     <div className="row g-3">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">email</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="email" value={data.email} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">password</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="password" value={data.password} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                           <button className="btn btn-success">login</button>
+                           <button className="btn btn-success" onClick={readvalue}>login</button>
                         </div>
-                        
+                        <div>
+                        <Link class="nav-link" to="/signup">signup</Link>
+                        </div>
                     </div>
                 </div>
             </div>
